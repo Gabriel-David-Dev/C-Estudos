a25b73f3c70e5a2083fd21749ad6599ee59a9606
Gabriel David <devgabrieldavid@gmail.com>
2023-12-04 17:14:32 -0300

    Feito algoritmo para comparar idades

---
+++ C# Projetos/ComparaIdade/ComparaIdade/Program.cs
@@ -0,0 +1,37 @@
+﻿using ComparaIdade.Entities;
+using ComparaIdade.Services;
+
+namespace ComparaIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                List<Pessoa> pessoas = new List<Pessoa>();
+                for (int contador = 1; contador <= 2; contador++)
+                {
+                    Console.Write($"Digite o nome da {contador}° pessoa: ");
+                    string nome = Console.ReadLine();
+                    Console.Write($"Digite a idade da {contador}° pessoa: ");
+                    int idade = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    Pessoa pessoa = new Pessoa(nome, idade);
+                    pessoas.Add(pessoa);
+
+                }
+
+                string resultado = VerificaIdadeServices.MaisVelha(pessoas[0], pessoas[1]);
+
+                Console.WriteLine($"Pessoa mais velha: {resultado}");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"Erro: {erro.Message}");
+            }
+        }
+    }
+}
---
+++ C# Projetos/ComparaIdade/ComparaIdade/Services/VerificaIdadeServices.cs
@@ -0,0 +1,23 @@
+﻿using ComparaIdade.Entities;
+
+namespace ComparaIdade.Services
+{
+    internal class VerificaIdadeServices
+    {
+        public static string MaisVelha(Pessoa pessoa1, Pessoa pessoa2)
+        {
+            if (pessoa1.Idade > pessoa2.Idade)
+            {
+                return pessoa1.Nome;
+            }
+            else if (pessoa2.Idade > pessoa1.Idade)
+            {
+                return pessoa2.Nome;
+            }
+            else
+            {
+                return "Possuem a mesma idade!";
+            }
+        }
+    }
+}
