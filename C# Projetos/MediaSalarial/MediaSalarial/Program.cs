1768814feb79861dc01b00b85579919c35f76fcc
Gabriel David <devgabrieldavid@gmail.com>
2023-12-04 17:29:46 -0300

    Feito algoritmo para media salarial de funcionarios

---
+++ C# Projetos/MediaSalarial/MediaSalarial/Program.cs
@@ -0,0 +1,39 @@
+﻿using MediaSalarial.Entities;
+using MediaSalarial.Services;
+using System.Globalization;
+
+namespace MediaSalarial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            try
+            {
+                for (int contador = 1; contador <= 2; contador++)
+                {
+                    Console.Write($"Digite o nome do {contador}° funcionário: ");
+                    string nome = Console.ReadLine();
+                    Console.Write($"Digite o salário do {contador}° funcionário: ");
+                    double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.WriteLine();
+
+                    Funcionario funcionario = new Funcionario(nome, salario);
+                    funcionarios.Add(funcionario);
+                }
+
+                double media = MediaSalario.Media(funcionarios[0].Salario, funcionarios[1].Salario);
+
+                Console.WriteLine($"Salário médio: {media.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"Erro: {erro.Message}");
+            }
+
+        }
+    }
+}
---
+++ C# Projetos/MediaSalarial/MediaSalarial/Services/MediaSalarioServices.cs
@@ -0,0 +1,10 @@
+﻿namespace MediaSalarial.Services
+{
+    internal class MediaSalario
+    {
+        public static double Media(double salario1, double salario2)
+        {
+            return (salario1 + salario2) / 2;
+        }
+    }
+}
