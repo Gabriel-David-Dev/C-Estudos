c3537f1820dabd7537671f860b1d7fb25be46737
Gabriel David <devgabrieldavid@gmail.com>
2023-12-05 06:51:03 -0300

    Feito algoritmo conversor de dolar para real

---
+++ C# Projetos/CompraDolar/CompraDolar/Program.cs
@@ -0,0 +1,29 @@
+﻿using CompraDolar.Services;
+using System.Globalization;
+
+namespace CompraDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Entre com a cotação do dólar: $");
+                double dolarPreco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantos dólares vão ser comprados: $");
+                double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double reais = CambioMoedaServices.ConversorDeMoeda(dolarPreco, dolar);
+
+                Console.WriteLine($"Valor a ser pago em reais: R${reais.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"Erro: {erro.Message}");
+            }
+            
+
+        }
+    }
+}
---
+++ C# Projetos/CompraDolar/CompraDolar/Services/CambioMoedaServices.cs
@@ -0,0 +1,13 @@
+﻿namespace CompraDolar.Services
+{
+    internal class CambioMoedaServices
+    {
+        public static double ConversorDeMoeda(double dolarPreco, double qtdDolar)
+        {
+            double imposto = 0.06;
+            double reais = dolarPreco * qtdDolar;
+            reais += + reais * imposto;
+            return reais;
+        }
+    }
+}
