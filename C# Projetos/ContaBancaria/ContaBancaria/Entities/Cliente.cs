e1d373ee121ee0c7fae2bb51477fa54b3148f0f0
Gabriel David <devgabrieldavid@gmail.com>
2023-12-05 07:45:07 -0300

    Feito algoritmo para criar conta bancária com algumas funções

---
+++ C# Projetos/ContaBancaria/ContaBancaria/Entities/Cliente.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace ContaBancaria.Entities
+{
+    internal class Cliente
+    {
+        public int NumeroConta { get; private set; }
+        public string TitularConta { get; set; }
+        public double Saldo { get; private set; }
+
+        public Cliente(int numeroConta, string titularConta, double saldo) 
+        {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+            Saldo = saldo;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        // É permitido ficar com saldo negativo apenas contando com a taxa de saque.
+        public void Saque(double valor)
+        {
+            double taxa = 5.0;
+            if (valor <= Saldo)
+            {
+                Saldo -= (valor + taxa);
+            }
+            else
+            {
+                Console.WriteLine("Valor Insuficiente !");
+            }
+        }
+
+        public void ExibirDados(string mensagem, Cliente cliente)
+        {
+            Console.WriteLine();
+            Console.WriteLine(mensagem);
+            Console.WriteLine(cliente);
+            Console.WriteLine();
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {TitularConta}, " +
+                $"Saldo: ${Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ C# Projetos/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,50 @@
+﻿using ContaBancaria.Entities;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Número da conta: ");
+                int numeroConta = int.Parse(Console.ReadLine());
+                Console.Write("Titular da conta: ");
+                string titularConta = Console.ReadLine();
+                Console.Write("Haverá depósito inicial (s/n): ");
+                char resposta = char.Parse(Console.ReadLine().ToLower());
+
+                double depositoInicial = 0.0;
+                if (resposta == 's')
+                {
+                    Console.Write("Valor de depósito inicial: ");
+                    depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+
+                Cliente cliente = new Cliente(numeroConta, titularConta, depositoInicial);
+
+                cliente.ExibirDados("Dados da conta", cliente);
+
+                Console.Write("Valor para depósito: ");
+                double depositar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                cliente.Deposito(depositar);
+
+                cliente.ExibirDados("Dados da conta atualizados", cliente);
+
+                Console.Write("Valor para saque: ");
+                double sacar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                cliente.Saque(sacar);
+
+                cliente.ExibirDados("Dados da conta atualizados", cliente);
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"Erro: {erro.Message}");
+            }
+            
+
+        }
+    }
+}
