b447e41a6ad1093525038cff3bb225a3be6700bf
Gabriel David <devgabrieldavid@gmail.com>
2023-12-06 06:14:43 -0300

    Feito algoritmo para gerenciar alugueis de quartos a estudantes

---
+++ C# Projetos/AlugaQuarto/AlugaQuarto/Entities/Estudante.cs
@@ -0,0 +1,21 @@
+﻿namespace AlugaQuarto.Entities
+{
+    internal class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Estudante(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            return $"{Quarto}: {Nome}, {Email}";
+        }
+    }
+}
---
+++ C# Projetos/AlugaQuarto/AlugaQuarto/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using AlugaQuarto.Entities;
+
+namespace AlugaQuarto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Estudante> estudantes = new List<Estudante>();
+
+            int[] quartos = new int[10];
+
+            try
+            {
+                Console.Write("Quantos quartos serão alugados: ");
+                int qtdQuartos = int.Parse(Console.ReadLine());
+
+                for (int contador = 1; contador <= qtdQuartos; contador++)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Aluguel #{contador}:");
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Email: ");
+                    string email = Console.ReadLine();
+                    Console.Write("Quarto: ");
+                    int quarto = int.Parse(Console.ReadLine());
+                    quartos[quarto] = quarto;
+
+                    Estudante estudante = new Estudante(nome, email, quartos[quarto]);
+                    estudantes.Add(estudante);
+                }
+
+                estudantes.Sort((a, b) => a.Quarto.CompareTo(b.Quarto));
+
+                Console.WriteLine();
+                Console.WriteLine("Quartos ocupados:");
+                foreach (Estudante estudante in estudantes)
+                {
+                    Console.WriteLine(estudante);
+                }
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"Ocorreu um ERRO: {erro.Message}");
+            }
+        }
+    }
+}
