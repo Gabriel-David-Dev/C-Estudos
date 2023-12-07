7b0e9551b0ce2575129a3e2792a0113edd471350
Gabriel David <devgabrieldavid@gmail.com>
2023-12-07 06:45:16 -0300

    Feito algoritmo para criar, preencher e verificar os valores de uma matriz

---
+++ C# Projetos/VarrendoMatriz/VarrendoMatriz/Program.cs
@@ -0,0 +1,30 @@
+﻿using VarrendoMatriz.Services;
+
+namespace VarrendoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                // Entrada de dados, criação e preenchimento da matriz
+                Console.Write("Entre com a quantidade de linhas da matriz: ");
+                int linhas = int.Parse(Console.ReadLine());
+                Console.Write("Entre com a quantidade de colunas da matriz: ");
+                int colunas = int.Parse(Console.ReadLine());
+
+                int[,] matriz = Matriz.CriaMatriz(linhas, colunas);
+
+                //  Exibir a matriz formatada
+                Matriz.ExibirMatriz(matriz);
+
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"Aconteceu um erro: {erro.Message}");
+            }
+        }
+    }
+}
---
+++ C# Projetos/VarrendoMatriz/VarrendoMatriz/Services/Matriz.cs
@@ -0,0 +1,48 @@
+﻿namespace VarrendoMatriz.Services
+{
+    internal class Matriz
+    {
+        // Metódo para criação e preenchimento da matriz
+        public static int[,] CriaMatriz(int numero1, int numero2)
+        {
+            int[,] matriz = new int[numero1, numero2];
+
+            for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                {
+                    matriz[linhas, colunas] = int.Parse(valores[colunas]);
+                }
+            }
+            return matriz;
+        }
+        
+        // Metódo para saída de dados, exibindo a diagonal principal e os números negativos
+        public static void ExibirMatriz(int[,] matriz)
+        {
+            Console.WriteLine();
+            int somaNegativos = 0;
+            Console.WriteLine("Main diagonal:");
+
+            for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+            {
+                for (int colunas = 0; colunas < matriz.GetLength(1); colunas++)
+                {
+                    if (linhas == colunas)
+                    {
+                        Console.Write($"{matriz[linhas, colunas]} ");
+                    }
+                    if (matriz[linhas, colunas] < 0)
+                    {
+                        somaNegativos++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Negative numbers: {somaNegativos}");
+
+        }
+    }
+}
