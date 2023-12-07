59bb06c67df2adef37b3c635838ddbe00dfa3f7c
Gabriel David <devgabrieldavid@gmail.com>
2023-12-07 08:11:39 -0300

    Feito algoritmo para capturar elementos específicos de uma matriz

---
+++ C# Projetos/ElementoMatriz/ElementoMatriz/Program.cs
@@ -0,0 +1,37 @@
+﻿using ElementoMatriz.Services;
+
+namespace ElementoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                // Cria e preenche matriz
+                Console.Write("Entre com a quantidade de linhas: ");
+                int linhas = int.Parse(Console.ReadLine());
+                Console.Write("Entre com a quantidade de colunas: ");
+                int colunas = int.Parse(Console.ReadLine());
+
+                int[,] matriz = Matriz.CriaMatriz(linhas, colunas);
+
+                // Exibe matriz e elementos próximos á um elemento informado
+                Console.WriteLine();
+                Console.Write("Escolha um dos elementos: ");
+                int escolha = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Matriz.ExibeEscolha(matriz, escolha);
+
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+            }
+
+
+        }
+    }
+}
---
+++ C# Projetos/ElementoMatriz/ElementoMatriz/Services/Matriz.cs
@@ -0,0 +1,58 @@
+﻿namespace ElementoMatriz.Services
+{
+    internal class Matriz
+    {
+        // Metodo para criar e preencher a matriz
+        public static int[,] CriaMatriz(int linhas, int colunas)
+        {
+            int[,] matriz = new int[linhas, colunas];
+
+            for (int contador = 0; contador < linhas; contador++)
+            {
+                string[] numeros = Console.ReadLine().Split(' ');
+
+                for (int cont = 0; cont < colunas; cont++)
+                {
+                    matriz[contador, cont] = int.Parse(numeros[cont]);
+                }
+            }
+
+            return matriz;
+        }
+
+        // Metodo para mostrar os elementos dado a escolha do usuario
+        public static void ExibeEscolha(int[,] matriz, int escolha)
+        {
+
+            for (int contador = 0; contador < matriz.GetLength(0); contador++)
+            {
+                for (int cont = 0; cont < matriz.GetLength(1); cont++)
+                {
+                    if (matriz[contador, cont] == escolha)
+                    {
+                        Console.WriteLine($"Posição - Linha {contador}, Coluna {cont}:");
+                        if (cont > 0)
+                        {
+                            Console.WriteLine($"Elemento a esquerda: {matriz[contador, cont - 1]}");
+                        }
+                        if (cont < matriz.GetLength(1) - 1)
+                        {
+                            Console.WriteLine($"Elemento a direita: {matriz[contador, cont + 1]}");
+                        }
+                        if (contador > 0)
+                        {
+                            Console.WriteLine($"Elemento acima: {matriz[contador - 1, cont]}");
+                        }
+                        if (contador < matriz.GetLength(0) - 1)
+                        {
+                            Console.WriteLine($"Elemento abaixo: {matriz[contador + 1, cont]}");
+                        }
+
+                        Console.WriteLine();
+                    }
+
+                }
+            }
+        }
+    }
+}
